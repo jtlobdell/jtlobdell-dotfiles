c154d27d1c68ebcc4851d876432a8479b26083cf
John Lobdell <john.lobdell@gmail.com>
2018-01-24 17:44:37 -0700

    fix syntax error in setup.sh

--- setup.sh
+++ setup.sh
@@ -5,6 +5,6 @@ ln -s `pwd`/.zshrc $HOME/.zshrc
 
 read -p "High DPI? (y/N)?" choice
 case "$choice" in
-	y|Y ) ln -s `pwd`/.Xresources-hidpi $HOME/.Xresources
-	* ) ln -s `pwd`/.Xresources $HOME/.Xresources
+	y|Y ) ln -s `pwd`/.Xresources-hidpi $HOME/.Xresources;;
+	* ) ln -s `pwd`/.Xresources $HOME/.Xresources;;
 esac
